5b5c29548b2167168613650c2ec73482b10f2c3f
Amanchaudary <amanecdy@gmail.com>
2024-05-02 11:55:58 +0900

    database に jpaproblam というデタベースが作りました

--- jpa/src/main/java/com/jpa/jpa/JpaApplication.java
+++ jpa/src/main/java/com/jpa/jpa/JpaApplication.java
@@ -2,12 +2,19 @@ package com.jpa.jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.jpa.jpa.enitity.entity;
 
 @SpringBootApplication
 public class JpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JpaApplication.class, args);
+		ApplicationContext context=SpringApplication.run(JpaApplication.class, args);
+		entity obj1= context.getBean(entity.class);
+		obj1.setId(112);
+		obj1.setName("chaudhary");
+		obj1.setAge(24);
 	}
 
 }
